cb8f593f7a643306da67ebf842ffe2a6bdb5ea20
Paweł Bulwan <git@taurit.pl>
2019-08-28 22:56:55 +0200

    Add support for "EnabledFilters" and "DisabledFilters" sheets

--- Taurit.Thunderbird.ExcelToCsvConverter/ExcelRulesReader.cs
+++ Taurit.Thunderbird.ExcelToCsvConverter/ExcelRulesReader.cs
@@ -11,7 +11,9 @@ namespace Taurit.Thunderbird.ExcelToCsvConverter
     {
         public IReadOnlyList<ExcelRule> Read(string excelRulesFileName)
         {
-            var result = new List<ExcelRule>();
+            var allRules = new List<ExcelRule>();
+            var enabledCategoriesNames = new List<string>();
+            var disabledCategoriesNames = new List<string>();
 
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             using var stream = File.Open(excelRulesFileName, FileMode.Open, FileAccess.Read);
@@ -22,21 +24,58 @@ namespace Taurit.Thunderbird.ExcelToCsvConverter
             {
                 while (reader.Read())
                 {
-                    var text = reader.GetString(0);
-                    var wholeWordsOnly = reader.GetString(1);
-                    var alsoMatchContent = reader.GetString(2);
-                    var category = reader.GetString(3);
+                    var sheetName = reader.Name;
 
-                    var excelRule = new ExcelRule(text, wholeWordsOnly, alsoMatchContent, category);
-                    result.Add(excelRule);
+                    if (sheetName == "Phrases")
+                    {
+                        allRules.Add(ReadExcelRule(reader));
+                    }
+                    else if (sheetName == "EnabledFilters")
+                    {
+                        var enabledCategoryName = reader.GetString(0);
+                        enabledCategoriesNames.Add(enabledCategoryName);
+                    }
+                    else if (sheetName == "DisabledFilters")
+                    {
+                        var disabledCatgoryName = reader.GetString(0);
+                        disabledCategoriesNames.Add(disabledCatgoryName);
+                    }
                 }
             } while (reader.NextResult());
 
-            if (result.Count != result.ToHashSet().Count)
+            if (allRules.Count != allRules.ToHashSet().Count)
                 throw new InvalidOperationException(
                     "Some rule is duplicated in excel file! sort by text and find a duplicate.");
 
-            return result;
+            // make sure that "enabled" and "disabled" list all rules
+            var duplicatedRules = enabledCategoriesNames.Intersect(disabledCategoriesNames).ToList();
+            if (duplicatedRules.Any())
+            {
+                throw new InvalidOperationException($"Some rules are present in both enabled and disabled lists. Fix `{String.Join(",", duplicatedRules)}`.");
+            }
+
+            var categoriesNames = allRules.Select(x => x.Category).ToHashSet().ToList();
+            foreach (var categoryName in categoriesNames)
+            {
+                if (!enabledCategoriesNames.Contains(categoryName) && !disabledCategoriesNames.Contains(categoryName))
+                {
+                    throw new InvalidOperationException($"Category `{categoryName}` is not present in EnabledRules nor DisabledRules sheets.");
+                }
+            }
+
+            var enabledCategoriesNamesHashSet = enabledCategoriesNames.ToHashSet();
+            return allRules.Where(x => enabledCategoriesNamesHashSet.Contains(x.Category)).ToList();
+        }
+
+        private static ExcelRule ReadExcelRule(IExcelDataReader reader)
+        {
+            var text = reader.GetString(0);
+            var wholeWordsOnly = reader.GetString(1);
+            var alsoMatchContent = reader.GetString(2);
+            var category = reader.GetString(3);
+
+            var excelRule = new ExcelRule(text, wholeWordsOnly, alsoMatchContent, category);
+            return excelRule;
         }
     }
 }
