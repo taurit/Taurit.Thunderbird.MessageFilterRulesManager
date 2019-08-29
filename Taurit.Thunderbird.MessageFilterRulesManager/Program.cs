0f92bbe704d1903e34556b7124147660a1e3fa3b
Paweł Bulwan <git@taurit.pl>
2019-08-29 19:15:16 +0200

    Add icon and basic argument validation

--- Taurit.Thunderbird.MessageFilterRulesManager/Program.cs
+++ Taurit.Thunderbird.MessageFilterRulesManager/Program.cs
@@ -10,10 +10,43 @@ namespace Taurit.Thunderbird.MessageFilterRulesManager
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TransformExcelFiltersToThunderbirdFilters(args);
+            }
+            catch (Exception e)
+            {
+                Logger.Log($"{e}");
+                Console.ReadLine();
+                throw;
+            }
+        }
+
+        private static void TransformExcelFiltersToThunderbirdFilters(string[] args)
+        {
             // read and validate arguments
+            if (args.Length != 2)
+            {
+                Logger.Log("You have not provided input arguments");
+                Console.ReadLine();
+                return;
+            }
+
             var excelRulesFileName = args[0];
             var thunderbirdRulesFileName = args[1];
-            
+            if (!File.Exists(excelRulesFileName))
+            {
+                Logger.Log("Excel file does not exist");
+                Console.ReadLine();
+                return;
+            }
+            if (!File.Exists(thunderbirdRulesFileName))
+            {
+                Logger.Log("Thunderbird file does not exist");
+                Console.ReadLine();
+                return;
+            }
+
             // make sure that thunderbird has not upgraded the format of the config file (from version 9)
             var msgFilterRulesFile = new FileParser().Parse(thunderbirdRulesFileName);
             var version = msgFilterRulesFile.Version;
@@ -40,8 +73,8 @@ namespace Taurit.Thunderbird.MessageFilterRulesManager
             FileWriter fileWriter = new FileWriter();
             var serializedModel = fileWriter.SerializeModel(tbModel);
             File.WriteAllText(thunderbirdRulesFileName, serializedModel);
+
+            Console.WriteLine("Done!");
         }
-
-
     }
 }
